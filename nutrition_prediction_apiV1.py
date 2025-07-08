557d035dbe52ea343df373c9f665741f1c18ce93
Lê Tấn Kim <89767092+Letankim@users.noreply.github.com>
2025-07-08 10:53:43 +0000

    Add main

--- nutrition_prediction_apiV1.py
+++ nutrition_prediction_apiV1.py
@@ -304,8 +304,3 @@ async def predict_nutrition_endpoint(
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Nutrition Prediction API. Use /predict/ to upload an image or provide a URL."}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     port = int(os.getenv("PORT", 8000))  # Render sẽ gán PORT, fallback 8000 khi chạy local
-#     uvicorn.run(app, host="0.0.0.0", port=port)
